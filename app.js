480445b2ef4240eda4f07933718ee3e69d1d4548
assist-siseam <assist.siseam@gmail.com>
2024-08-26 13:54:13 +0600

    apsoc

---
+++ app.js
@@ -0,0 +1,15 @@
+var counDate = new Date("Jun 10, 2025 00:00:00").getTime();
+var x = setinterval(function () {
+  var now = new Date().getTime();
+  var distance = counDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+}, 1000);
